bd25e293cd9446c8811a5899dc31f65ff469c02d
Jakub Szymankiewicz <jszymankiewicz@INF.UG.EDU.PL>
2025-10-15 16:04:04 +0200

    Commit UG 2025.10.15

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,6 +3,8 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "1234567891", "PROMO_123")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
+        assert account.balance == 50.0
+        assert account.pesel == "Invalid"
